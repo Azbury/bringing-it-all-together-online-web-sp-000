1b94e510ccdc91cb78f57ef9ce77249b08dd4659
Austin Asbury <acasbury25@aol.com>
2019-09-24 20:28:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,4 +58,11 @@ class Dog
     end
     found_dog
   end
+
+  def self.find_by_name (name)
+    DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 end
