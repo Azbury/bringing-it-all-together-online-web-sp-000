63e9f270b1fc030bc4d6bce0acd2d44e3d5a6798
Austin Asbury <acasbury25@aol.com>
2019-09-24 20:20:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,4 +43,10 @@ class Dog
     self.new({:id => row[0], :name => row[1], :breed => row[2]})
   end
 
+  def self.find_by_id (num)
+    DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", num).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 end
